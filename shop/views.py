c4c07639eb5d385aa553b60f3fd0a1a70c3af54e
rahim-think <rahim.aghareb@gmail.com>
2021-01-28 13:47:20 +0330

    files modified to render templates

--- shop/views.py
+++ shop/views.py
@@ -5,6 +5,7 @@ from .models import Category, Product
 def product_list(requset, category_slug=None):
     category = None
     categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
