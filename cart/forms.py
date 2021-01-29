187e87b831cbbb9136c6e30d18145d6878156a58
rahim-think <rahim.aghareb@gmail.com>
2021-01-29 12:10:55 +0330

    forms added to cart app

---
+++ cart/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+# the min and max quantity user could buy
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
+
+
+class CartAppProductForm(forms.Form):
+    quantity = forms.TypedChoiceField(
+        choices=PRODUCT_QUANTITY_CHOICES,
+        coerce=int)  # convert input to integer
+
+    # override indicates whether the quantity should update or not
+    override = forms.BooleanField(required=False,
+                                  initial=False,
+                                  widget=forms.HiddenInput)  # we do not like to show it to the user
