3ed99d14858539e070089b22acb951a7c5245c6c
rahim-think <rahim.aghareb@gmail.com>
2021-01-29 13:17:04 +0330

    some minor typos edited

--- cart/forms.py
+++ cart/forms.py
@@ -4,7 +4,7 @@ from django import forms
 PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
-class CartAppProductForm(forms.Form):
+class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(
         choices=PRODUCT_QUANTITY_CHOICES,
         coerce=int)  # convert input to integer
--- cart/views.py
+++ cart/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from shop.models import Product
 from .cart import Cart
-from .forms import CartAppProductForm
+from .forms import CartAddProductForm
 
 
 @require_POST
 def cart_add(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
-    form = CartAppProductForm(request.Post)
+    form = CartAddProductForm(request.POST)
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product,
