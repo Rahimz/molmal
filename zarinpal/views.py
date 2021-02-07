15a7113df813ab6ea75f1bc46c2c50dbf5667ab6
rahim-think <rahim.aghareb@gmail.com>
2021-02-07 14:48:22 +0330

    sending pdf invoice after  successful payment added to Celery tasks

--- zarinpal/views.py
+++ zarinpal/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import redirect, get_object_or_404
 from zeep import Client
 from orders.models import Order
+from .tasks import payment_completed
 
 
 MERCHANT = 'XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX'
 client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
-
+# we use this variable to edit order after the successful payment
+paid_order = None
 
 
 def send_request(request):
@@ -14,8 +16,10 @@ def send_request(request):
     order_id = request.session.get('order_id')
     order = get_object_or_404(Order, id=order_id)
     amount = order.get_total_cost()  # Toman / Required
-    print(amount)
-
+    #print(amount)
+    
+    paid_order = order
+    
     description = "توضیحات مربوط به تراکنش را در این قسمت وارد کنید"  # Required
     email = 'email@example.com'  # Optional
     mobile = '09123456789'  # Optional
@@ -33,6 +37,11 @@ def verify(request):
     if request.GET.get('Status') == 'OK':
         result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
         if result.Status == 100:
+            # we change the order status after successful payment
+            paid_order.paid = True
+            paid_order.save()
+            # launch asynchronous task
+            payment_completed.delay(paid_order.id)
             return HttpResponse('Transaction success.\nRefID: ' + str(result.RefID))
         elif result.Status == 101:
             return HttpResponse('Transaction submitted : ' + str(result.Status))
