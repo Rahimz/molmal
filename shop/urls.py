e52931cd8765da5226347c625743614b49936d7c
rahim <rahim.aghareb@gmail.com>
2021-07-18 22:56:05 +0430

    urls cache for home page added

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from . import views
 
 
@@ -9,5 +10,5 @@ urlpatterns = [
     path('prices/', views.price_view, name='price_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-    path('', views.home, name='home'),
+    path('', cache_page(60 * 15)(views.home), name='home'),
 ]
