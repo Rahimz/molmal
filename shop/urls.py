e7d3493658994b704d14de94b53735d645bcb08c
rahim <rahim.aghareb@gmail.com>
2021-08-04 14:10:16 +0430

    [BUG] Farsi slug does not work

--- shop/urls.py
+++ shop/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 from django.views.decorators.cache import cache_page
 from django.conf import settings
 from . import views
@@ -13,6 +13,8 @@ if settings.DEBUG:
         path('prices/', views.price_view, name='price_list'),
         path('search/', views.product_search, name='product_search'),
         path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+        # TODO:: Farsi slug does not work
+        # re_path(r'(?P<category_slug>\w+)$', views.product_list, name='product_list_by_category'),
         path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
         path('', views.home, name='home'),
     ]
