0164a0337970ad55343769b633a0c6ff1d966b27
rahim <rahim.aghareb@gmail.com>
2021-08-05 10:47:01 +0430

    urls change to fix the product detail issu

--- shop/urls.py
+++ shop/urls.py
@@ -23,7 +23,7 @@ else:
         path('products/', views.product_list, name='product_list'),
         path('prices/', views.price_view, name='price_list'),
         path('search/', views.product_search, name='product_search'),
-        path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
         path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+        path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
         path('', cache_page(60 * 15)(views.home), name='home'),
     ]
