8ab8af44be57d1ed3ab4ea1112eefd703827dba6
rahim <rahim.aghareb@gmail.com>
2021-09-14 10:51:30 +0430

    fa-date in orders model changed to null and blank true

--- orders/models.py
+++ orders/models.py
@@ -13,7 +13,7 @@ class Order(models.Model):
     last_name = models.CharField(_('last name'),
                                  max_length=50)
     phone = models.CharField(max_length=12, blank=True, null=True)
-    fa_date = jmodels.jDateField()
+    fa_date = jmodels.jDateField(null=True, blank=True)
     email = models.EmailField(_('e-mail'), blank=True, null=True)
     address = models.CharField(_('address'), max_length=250, blank=True, null=True)
     postal_code = models.CharField(_('postal code'), max_length=20, blank=True, null=True)
@@ -24,23 +24,23 @@ class Order(models.Model):
     delivered = models.BooleanField(default=False)
 
     coupon = models.ForeignKey(Coupon,
-                               related_name='orders', 
+                               related_name='orders',
                                null=True,
                                blank=True,
-                               on_delete=models.SET_NULL) 
-                               # if the coupon gets deleted, 
-                               # the coupon field is set to Null, 
+                               on_delete=models.SET_NULL)
+                               # if the coupon gets deleted,
+                               # the coupon field is set to Null,
                                # but the discount is preserved
     discount = models.IntegerField(default=0,
                                    validators=[MinValueValidator(0),
                                                MaxValueValidator(100)])
-    
+
     class Meta:
         ordering = ('-created',)
-    
+
     def __str__(self):
         return f'Order {self.id}'
-    
+
     def get_total_cost(self):
         total_cost = sum(item.get_cost() for item in self.items.all())
         return total_cost - total_cost * (self.discount / Decimal(100))
@@ -55,9 +55,9 @@ class OrderItem(models.Model):
                                 on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveIntegerField(default=1)
-    
+
     def __str__(self):
         return str(self.id)
-    
+
     def get_cost(self):
         return self.price * self.quantity
