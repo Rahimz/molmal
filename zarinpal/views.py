633b6c08bde61beb1f8eb0fc881c9b8e3c82bb32
rahim <rahim.aghareb@gmail.com>
2021-09-19 20:53:53 +0430

    callbck view for zarinpal edited

--- zarinpal/views.py
+++ zarinpal/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from zeep import Client
 from orders.models import Order
 from .tasks import payment_completed
@@ -12,7 +12,7 @@ MERCHANT = merchant
 ZP_API_REQUEST = "https://api.zarinpal.com/pg/v4/payment/request.json"
 ZP_API_VERIFY = "https://api.zarinpal.com/pg/v4/payment/verify.json"
 ZP_API_STARTPAY = "https://www.zarinpal.com/pg/StartPay/{authority}"
-CallbackURL = 'http://localhost:8000/verify/' # Important: need to edit for realy server.
+CallbackURL = 'http://localhost:8000/zarinpal/verify/' # Important: need to edit for realy server.
 # client = Client('https://www.zarinpal.com/pg/services/WebGate/wsdl')
 # we use this variable to edit order after the successful payment
 paid_order = None
@@ -83,30 +83,28 @@ def verify(request):
                     req.json()['data']['message']
                 ))
             else:
-                return HttpResponse('Transaction failed.\nStatus: ' + str(
-                    req.json()['data']['message']
-                ))
+                return render(request,
+                              'zarinpal/fail.html',
+                              {'message': str (req.json()['data']['message'])})
+                # return HttpResponse('Transaction failed.\nStatus: ' +
+                #                     str(req.json()['data']['message']))
         else:
             e_code = req.json()['errors']['code']
             e_message = req.json()['errors']['message']
-            return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
+            return render(request,
+                          'zarinpal/fail.html',
+                          {'message': f"Error code: {e_code}, Error Message: {e_message}"})
+            # return HttpResponse(f"Error code: {e_code}, Error Message: {e_message}")
     else:
+        # req_header = {"accept": "application/json",
+        #               "content-type": "application/json'"}
+        # req_data = {
+        #     "merchant_id": MERCHANT,
+        #     "amount": amount,
+        #     "authority": t_authority
+        # }
+        # req = requests.post(url=ZP_API_VERIFY, data=json.dumps(req_data), headers=req_header)
+        # return render(request,
+        #               'zarinpal/fail.html',
+        #               {'message': ''})
         return HttpResponse('Transaction failed or canceled by user')
-
-
-# def verify(request):
-#     if request.GET.get('Status') == 'OK':
-#         result = client.service.PaymentVerification(MERCHANT, request.GET['Authority'], amount)
-#         if result.Status == 100:
-#             # we change the order status after successful payment
-#             paid_order.paid = True
-#             paid_order.save()
-#             # launch asynchronous task
-#             payment_completed.delay(paid_order.id)
-#             return HttpResponse('Transaction success.\nRefID: ' + str(result.RefID))
-#         elif result.Status == 101:
-#             return HttpResponse('Transaction submitted : ' + str(result.Status))
-#         else:
-#             return HttpResponse('Transaction failed.\nStatus: ' + str(result.Status))
-#     else:
-#         return HttpResponse('Transaction failed or canceled by user')
