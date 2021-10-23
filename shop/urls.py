01983c5c2887433cd35e05fcb1253f0c4efa4047
rahim <rahim.aghareb@gmail.com>
2021-10-23 19:45:53 +0330

    cache system disabled due to cart could not update in cahcheed views

--- shop/urls.py
+++ shop/urls.py
@@ -5,25 +5,36 @@ from . import views
 
 
 app_name = 'shop'
+# cache page omitted because when we cache all the url
+# cart does not upgrade
+# # TODO: Cache should be in template or in views to solve this problem
+urlpatterns = [
+    path('products/', views.product_list, name='product_list'),
+    path('prices/', views.price_view, name='price_list'),
+    path('search/', views.product_search, name='product_search'),
+    path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+    path('', views.home, name='home'),
+]
 
 # THe if statement is for disableing cache in local env
-if settings.DEBUG:
-    urlpatterns = [
-        path('products/', views.product_list, name='product_list'),
-        path('prices/', views.price_view, name='price_list'),
-        path('search/', views.product_search, name='product_search'),
-        path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-        # TODO:: Farsi slug does not work
-        # re_path(r'(?P<category_slug>\w+)$', views.product_list, name='product_list_by_category'),
-        path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-        path('', views.home, name='home'),
-    ]
-else:
-    urlpatterns = [
-        path('products/', cache_page(60 * 10)(views.product_list), name='product_list'),
-        path('prices/', views.price_view, name='price_list'),
-        path('search/', views.product_search, name='product_search'),
-        path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
-        path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
-        path('', cache_page(60 * 10)(views.home), name='home'),
-    ]
+# if settings.DEBUG:
+#     urlpatterns = [
+#         path('products/', views.product_list, name='product_list'),
+#         path('prices/', views.price_view, name='price_list'),
+#         path('search/', views.product_search, name='product_search'),
+#         path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+#         # TODO:: Farsi slug does not work
+#         # re_path(r'(?P<category_slug>\w+)$', views.product_list, name='product_list_by_category'),
+#         path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+#         path('', views.home, name='home'),
+#     ]
+# else:
+#     urlpatterns = [
+#         path('products/', cache_page(60 * 10)(views.product_list), name='product_list'),
+#         path('prices/', views.price_view, name='price_list'),
+#         path('search/', views.product_search, name='product_search'),
+#         path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+#         path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
+#         path('', cache_page(60 * 10)(views.home), name='home'),
+#     ]
