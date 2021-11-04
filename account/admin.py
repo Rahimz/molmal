6ff9ec12b23109ddf029af8da820a970535ac75d
rahim <rahim.aghareb@gmail.com>
2021-11-04 15:22:03 +0330

    address model added to account

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Address
 
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ['user', 'date_of_birth', 'photo']
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['user', 'fav_address', 'address']
--- account/models.py
+++ account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 import datetime
+from django.utils.translation import gettext_lazy as _
 
 
 class Profile(models.Model):
@@ -24,3 +25,20 @@ class Profile(models.Model):
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
+
+class Address(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                      on_delete=models.CASCADE,
+                      related_name='address')
+    fav_address = models.BooleanField(default=False)
+    first_name = models.CharField(_('first name'),
+                                  max_length=50, blank=True, null=True)
+    last_name = models.CharField(_('last name'),
+                                 max_length=50)
+    phone = models.CharField(_('Phone'),max_length=12)
+    address = models.CharField(_('address'), max_length=250)
+    postal_code = models.CharField(_('postal code'), max_length=20)
+    city = models.CharField(_('city'), max_length=100)
+
+    def __str__(self):
+        return self.address[:30]
